5a0ec7754037a739f63ea5ea5cc95a3757006c54
ngochh <ngochh@northstudio.vn>
2025-04-22 19:25:06 +0700

    :sparkles: implement text list

--- src/components/custom-tiptap/toolbar/index.tsx
+++ src/components/custom-tiptap/toolbar/index.tsx
@@ -4,9 +4,10 @@ import { Separator } from "@/components/ui/separator"
 import TextStyle from "@/components/custom-tiptap/toolbar/text-style";
 import {TextColor} from "@/components/custom-tiptap/toolbar/text-color";
 import {TextHeading} from "@/components/custom-tiptap/toolbar/text-heading";
+import {TextList} from "@/components/custom-tiptap/toolbar/text-list";
 
 export const EditorToolbar = ({ editor }: { editor: Editor }) => (
-  <div className="bg-muted/50 p-2 flex flex-wrap gap-1 items-center border-b">
+  <div className="p-2 flex flex-wrap gap-1 items-center border-b">
     <div className="flex w-max items-center">
       <TextStyle
         editor={editor}
@@ -23,6 +24,7 @@ export const EditorToolbar = ({ editor }: { editor: Editor }) => (
       <Separator orientation="vertical" className="mx-2 h-7" />
 
       <TextHeading editor={editor} activeLevels={[1, 2, 3, 4]}/>
+      <TextList editor={editor} />
       <TextColor editor={editor} />
 
     </div>
---
+++ src/components/custom-tiptap/toolbar/text-list.tsx
@@ -0,0 +1,72 @@
+import * as React from "react"
+import type { Editor } from "@tiptap/react"
+import { ListBulletIcon } from "@radix-ui/react-icons"
+import {EditorFormatAction} from "@/components/custom-tiptap/editor-format-action";
+import ToolbarButton from "@/components/custom-tiptap/toolbar-button";
+
+type ListItemAction = "orderedList" | "bulletList"
+interface ListItem extends Omit<EditorFormatAction, 'shortcuts'> {
+  value: ListItemAction
+}
+
+const formatActions: ListItem[] = [
+  {
+    value: "orderedList",
+    label: "Numbered list",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="20px"
+        viewBox="0 -960 960 960"
+        width="15px"
+        fill="currentColor"
+      >
+        <path d="M144-144v-48h96v-24h-48v-48h48v-24h-96v-48h120q10.2 0 17.1 6.9 6.9 6.9 6.9 17.1v48q0 10.2-6.9 17.1-6.9 6.9-17.1 6.9 10.2 0 17.1 6.9 6.9 6.9 6.9 17.1v48q0 10.2-6.9 17.1-6.9 6.9-17.1 6.9H144Zm0-240v-96q0-10.2 6.9-17.1 6.9-6.9 17.1-6.9h72v-24h-96v-48h120q10.2 0 17.1 6.9 6.9 6.9 6.9 17.1v72q0 10.2-6.9 17.1-6.9 6.9-17.1 6.9h-72v24h96v48H144Zm48-240v-144h-48v-48h96v192h-48Zm168 384v-72h456v72H360Zm0-204v-72h456v72H360Zm0-204v-72h456v72H360Z" />
+      </svg>
+    ),
+    isActive: (editor) => editor.isActive("orderedList"),
+    action: (editor) => editor.chain().focus().toggleOrderedList().run(),
+    canExecute: (editor) => editor.can().chain().focus().toggleOrderedList().run(),
+  },
+  {
+    value: "bulletList",
+    label: "Bullet list",
+    icon: <ListBulletIcon className="size-5" />,
+    isActive: (editor) => editor.isActive("bulletList"),
+    action: (editor) => editor.chain().focus().toggleBulletList().run(),
+    canExecute: (editor) => editor.can().chain().focus().toggleBulletList().run(),
+  },
+]
+
+interface TextListProps {
+  editor: Editor
+  activeActions?: ListItemAction[]
+}
+
+export const TextList = (props: TextListProps) => {
+  const {editor, activeActions = formatActions.map((action) => action.value)} = props
+  const renderToolbarButton = React.useCallback(
+    (actionValue: ListItemAction) => {
+      const action = formatActions.find((a) => a.value === actionValue)
+      if (!action) return null
+
+      return (
+        <ToolbarButton
+          key={action.label}
+          onClick={() => action.action(editor)}
+          isActive={action.isActive(editor)}
+          tooltip={`${action.label}`}
+          aria-label={action.label}
+        >
+          {action.icon}
+        </ToolbarButton>
+      )
+    },
+    [editor]
+  )
+  return (
+    <>
+      {activeActions.map(renderToolbarButton)}
+    </>
+  )
+}
